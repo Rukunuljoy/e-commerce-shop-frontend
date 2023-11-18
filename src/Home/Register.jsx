4074d549bba892afe59f4f233503c1569787be48
Rukunuljoy <rukunuljoypub58@gmail.com>
2023-11-18 17:33:20 +0600

    category shjowcase is complete

---
+++ src/Home/Register.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const subTitle = "Save the day"
+const title = (
+    <h2>Lorem, ipsum dolor sit amet consectetur <b>advance <span>Mastering</span></b>on sales</h2>
+)
+
+const Register = () => {
+    return (
+        <div>
+            Register
+        </div>
+    );
+};
+
+export default Register;
---
+++ src/components/Rating.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Rating = () => {
+    return (
+        <div className='rating'>
+           <i className="icofont-ui-rating"></i> 
+           <i className="icofont-ui-rating"></i>
+           <i className="icofont-ui-rating"></i>
+           <i className="icofont-ui-rating"></i>
+           <i className="icofont-ui-rating"></i>
+        </div>
+    );
+};
+
+export default Rating;
