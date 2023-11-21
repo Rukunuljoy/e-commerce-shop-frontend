f011bb9685bbd00135975d09bcb44338fd62a8c6
Rukunuljoy <rukunuljoypub58@gmail.com>
2023-11-21 16:50:15 +0600

    pagination is running

--- src/shop/Shop.jsx
+++ src/shop/Shop.jsx
@@ -1,10 +1,27 @@
 import { useState } from "react";
 import PageHeader from "../components/PageHeader";
+import Data from '../products.json'
+import ProductCards from "./ProductCards";
+import Pagination from "./Pagination";
 
 const showResults = "Showing 01- 12 of Results";
 
 const Shop = () => {
-    const [GridList,setGridList] = useState(true);
+    const [GridList,setGridList] = useState(false);
+    const [products, setProducts] = useState(Data)
+
+    // pagination 
+    const [currentPage, setCurrentPage] = useState(1)
+    const productsPerPage = 12 
+
+    const indexOfLastProduct = currentPage * productsPerPage
+    const indexOfFirstProduct = indexOfLastProduct * productsPerPage
+    const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
+
+    // function to change the currentPage
+    const paginate = (pageNumber) =>{
+        setCurrentPage(pageNumber)
+    }
   return (
     <div>
       <PageHeader title="our Shop page" curPage="page" />
@@ -18,14 +35,21 @@ const Shop = () => {
                         <div className="shop-title d-flex flex-wrap justify-content-between">
                             <p>{showResults}</p>
                             <div className={`product-view-mode ${GridList ? 'gridActive' : 'listActive'}`}>
-                                <a href="grid" onClick={()=>setGridList(!GridList)}>
+                                <a className="grid" onClick={()=>setGridList(!GridList)}>
                                     <i className="icofont-ghost"></i>
                                 </a>
-                                <a href="list" onClick={()=>setGridList(!GridList)}>
+                                <a className="list" onClick={()=>setGridList(!products)}>
                                     <i className="icofont-listine-dots"></i>
                                 </a>
                             </div>
                         </div>
+                        {/* products card */}
+                        <div>
+                            <ProductCards GridList={GridList} products={products}/>
+                        </div>
+                        <Pagination
+                        
+                        />
                     </article>
                 </div>
                 <div className="col-lg-4 col-12">
